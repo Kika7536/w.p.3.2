c39d5d4d6b97d99ea7b6bc34388fe0028e23f906
Kika7536 <kika7536.p@gmail.com>
2024-09-08 18:20:12 +0300

    Добавьте файлы проекта.

---
+++ w.p.3.2/консоля.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+
+
+void main() {
+	setlocale(LC_ALL, "rus");
+
+	int a, b, c;
+
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+	cout << "Введите их сумму: ";
+	cin >> c;
+
+	cout << "-----Проверяем-----" << endl;
+
+	if (a + b == c) {
+		cout << "Верно!" << endl;
+	}
+	else {
+		cout << "Ошибка! Верный результат: " << a + b << endl;
+	}
+}
